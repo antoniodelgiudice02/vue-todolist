ad66b16d935fdc1125ecc84e7479b1abee8d0e92
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-31 18:18:30 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+
+        return {
+            tasks: [
+                {
+                    text: 'task 1',
+                    done: true,
+                },
+                {
+                    text: 'task 2',
+                    done: false,
+                },
+                {
+                    text: 'task 3',
+                    done: true,
+                },
+                {
+                    text: 'task 4',
+                    done: false,
+                },
+                {
+                    text: 'task 5',
+                    done: true,
+                }
+            ]
+        }
+
+    }
+}).mount('#app')
